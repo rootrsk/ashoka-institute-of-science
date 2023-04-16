1cfea21e832f7c1c3b19fb9d5181c8e4e04ef19e
rootrsk <ravishankar7050@gmail.com>
2023-04-16 19:32:46 +0530

    adding cors

--- server/app.js
+++ server/app.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser')
 const useragent = require('express-useragent')
 const cookieParser = require('cookie-parser')
 const db = require("./src/config/db")
+const cors = require("cors")
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001
 const userRouter = require('./src/routes/user')
 const adminRouter = require('./src/routes/admin')
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(useragent.express())
 app.use(cookieParser())
